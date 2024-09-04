21e6d7bf971eb09663db7eb9ccf4ae08fb7b2c23
Zachariah Russell <russellz1005@gmail.com>
2024-09-04 15:27:32 -0400

    Added method to handle BufferedImages

--- src/qrcodeapi/Application.java
+++ src/qrcodeapi/Application.java
@@ -3,9 +3,21 @@ package qrcodeapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//All following imports is to convert BufferedImage
+import java.awt.image.BufferedImage;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.BufferedImageHttpMessageConverter;
+import org.springframework.http.converter.HttpMessageConverter;
+
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
+    @Bean
+    public HttpMessageConverter<BufferedImage> bufferedImageHttpMessageConverter() {
+        return new BufferedImageHttpMessageConverter();
+    }
+
 }
