d1b5a7f481c40cb6292fd103eb17e6f813504ce7
Zachariah Russell <26415974+Russellzj@users.noreply.github.com>
2024-09-04 15:28:17 -0400

    Initi Commit

--- src/qrcodeapi/QrCodeGenerator.java
+++ src/qrcodeapi/QrCodeGenerator.java
@@ -1,2 +1,16 @@
-package qrcodeapi;public class QrCodeGenerator {
+package qrcodeapi;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+
+public class QrCodeGenerator {
+    public static BufferedImage createQrCode() {
+        BufferedImage image = new BufferedImage(250, 250, BufferedImage.TYPE_INT_RGB);
+        Graphics2D square = image.createGraphics();
+        square.setColor(Color.WHITE);
+        square.fillRect(0, 0, 250, 250);
+        square.dispose();
+        return image;
+    }
 }
