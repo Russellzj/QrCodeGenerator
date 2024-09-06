5f25bd8335b4466b506d17065577b8923fede7d8
Zachariah Russell <26415974+Russellzj@users.noreply.github.com>
2024-09-05 23:52:48 -0400

    Changed so that size will be chosen by the user and that size will be checked against the max and min requirements

--- src/qrcodeapi/QrCodeGenerator.java
+++ src/qrcodeapi/QrCodeGenerator.java
@@ -5,11 +5,35 @@ import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 
 public class QrCodeGenerator {
-    public static BufferedImage createQrCode() {
-        BufferedImage image = new BufferedImage(250, 250, BufferedImage.TYPE_INT_RGB);
+    private final int maxSize = 350;
+    private final int minSize = 150;
+    private int size;
+
+
+    public boolean setSize(int size) {
+        if (size > maxSize) {
+            return false;
+        }
+        if (size < minSize) {
+            return false;
+        }
+        this.size = size;
+        return true;
+    }
+
+    public int getMaxSize() {
+        return maxSize;
+    }
+
+    public int getMinSize() {
+        return minSize;
+    }
+
+    public BufferedImage createQrCode() {
+        BufferedImage image = new BufferedImage(size, size, BufferedImage.TYPE_INT_RGB);
         Graphics2D square = image.createGraphics();
         square.setColor(Color.WHITE);
-        square.fillRect(0, 0, 250, 250);
+        square.fillRect(0, 0, size, size);
         square.dispose();
         return image;
     }
