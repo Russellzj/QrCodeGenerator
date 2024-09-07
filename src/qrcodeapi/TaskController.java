426a42fa9d2b0a18217e90bde463c95b16077de2
Zachariah Russell <26415974+Russellzj@users.noreply.github.com>
2024-09-07 19:47:28 -0400

    Working on getting JavaScript Response

--- src/qrcodeapi/TaskController.java
+++ src/qrcodeapi/TaskController.java
@@ -1,4 +1,5 @@
 package qrcodeapi;
+import org.apache.tomcat.util.json.JSONParserTokenManager;
 import org.springframework.boot.actuate.autoconfigure.observation.ObservationProperties;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -7,6 +8,7 @@ import org.springframework.scheduling.config.Task;
 import org.springframework.web.bind.annotation.*;
 
 import java.awt.image.BufferedImage;
+import java.util.List;
 
 @RestController
 public class TaskController {
@@ -27,11 +29,14 @@ public class TaskController {
                                     @RequestParam String type) {
         QrCodeGenerator qrCodeSquare = new QrCodeGenerator();
         if (!qrCodeSquare.setSize(size)) {
+
+            String errorMessage = String.format("Image size must be between %d and %d pixels",
+                    qrCodeSquare.getMinSize(),
+                    qrCodeSquare.getMaxSize());
+            List<String> errorList = List.of(errorMessage);
             return ResponseEntity.badRequest().
                     contentType(MediaType.APPLICATION_JSON).
-                    body(String.format("error : Image size must be between %d and %d pixels",
-                            qrCodeSquare.getMinSize(),
-                            qrCodeSquare.getMaxSize()));
+                    body(errorList.get(0));
         } else {
             return ResponseEntity.ok()
                     .contentType(MediaType.parseMediaType("image/" + type))
