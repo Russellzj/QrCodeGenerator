77a9af2be501caa7d718224f3fa97c7ae0d3065c
Zachariah Russell <26415974+Russellzj@users.noreply.github.com>
2024-09-08 14:05:25 -0400

    Added correction level variable

--- src/qrcodeapi/QrCodeGenerator.java
+++ src/qrcodeapi/QrCodeGenerator.java
@@ -4,6 +4,7 @@ import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.util.List;
+import java.util.Map;
 
 //Imports for QRCode Generation
 import com.google.zxing.BarcodeFormat;
@@ -12,12 +13,18 @@ import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
 
+//Imports for Error Correction Levels
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+
+
 public class QrCodeGenerator {
     private final int maxSize = 350;
     private final int minSize = 150;
     private int size;
 
     private String type;
+    private char correctionLevel;
 
 
     public boolean setSize(int size) {
@@ -52,6 +59,16 @@ public class QrCodeGenerator {
         return minSize;
     }
 
+    public boolean setCorrectionLevel(char correctionLevel) {
+        List<Character> levels = List.of('L', 'M', 'Q', 'H');
+        correctionLevel = Character.toUpperCase(correctionLevel);
+        if (levels.contains(correctionLevel)) {
+            this.correctionLevel = correctionLevel;
+            return true;
+        }
+        return false;
+    }
+
     public BufferedImage createQrCodeBlank() {
         BufferedImage image = new BufferedImage(size, size, BufferedImage.TYPE_INT_RGB);
         Graphics2D square = image.createGraphics();
@@ -62,11 +79,17 @@ public class QrCodeGenerator {
     }
 
     public BufferedImage createQrCode(String address) {
+        Map<Character, ErrorCorrectionLevel> levels = Map.of(
+                'L', ErrorCorrectionLevel.L,
+                'M', ErrorCorrectionLevel.M,
+                'Q', ErrorCorrectionLevel.Q,
+                'H', ErrorCorrectionLevel.H);
         QRCodeWriter writer = new QRCodeWriter();
         BufferedImage defaultImage = createQrCodeBlank();
-
+        Map<EncodeHintType, ErrorCorrectionLevel> errorCorrectionLevel =
+                Map.of(EncodeHintType.ERROR_CORRECTION, levels.get(correctionLevel));
         try {
-            BitMatrix bitMatrix = writer.encode(address, BarcodeFormat.QR_CODE, size, size);
+            BitMatrix bitMatrix = writer.encode(address, BarcodeFormat.QR_CODE, size, size, errorCorrectionLevel);
             return MatrixToImageWriter.toBufferedImage(bitMatrix);
         } catch (WriterException e) {
             System.out.println(e.getMessage());
